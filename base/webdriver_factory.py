dac7a59605d9a4074de87873b28bda8668dfdef9
Kristopher Todd <kjtodd37@gmail.com>
2019-01-02 13:37:06 -0500

    Updated webdriver settings in the webdriver_factory.py file

--- base/webdriver_factory.py
+++ base/webdriver_factory.py
@@ -38,9 +38,9 @@ class WebDriverFactory:
         Returns:
             'WebDriver Instance'
         """
+        # base_url = "http://a06543.sys.ds.wolseley.com:3043/psp/fusdev10/"  # FUSDEV10
         # base_url = "http://a06615.sys.ds.wolseley.com:3050/psp/fustst00/"  # FUSTST00
         # base_url = "http://a06615.sys.ds.wolseley.com:3052/psc/fustst10/"  # FUSTST10
-        # base_url = "http://a06543.sys.ds.wolseley.com:3043/psp/fusdev10/"  # FUSDEV10
         base_url = "http://a06617.sys.ds.wolseley.com:3061/psp/fusuat10/"  # FUSUAT10
 
         if self.browser == "iexplorer":
